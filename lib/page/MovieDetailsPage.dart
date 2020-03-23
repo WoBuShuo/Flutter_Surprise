22d3403d694dd05c486dace765a16b08df37b067
li sao <1318213707@qq.com>
2020-03-23 23:38:33 +0800

    合并2

--- lib/page/MovieDetailsPage.dart
+++ lib/page/MovieDetailsPage.dart
@@ -5,11 +5,10 @@ import 'package:widget_chain/widget_chain.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MovieDetailsPage extends StatefulWidget{
-
+class MovieDetailsPage extends StatefulWidget {
   String movieID;
-  MovieDetailsPage(this.movieID);
 
+  MovieDetailsPage(this.movieID);
 
   @override
   State<StatefulWidget> createState() {
@@ -19,28 +18,26 @@ class MovieDetailsPage extends StatefulWidget{
 
 class MovieDetailsState extends State<MovieDetailsPage> {
   String movieID;
-  MovieDetailsState(this.movieID);
 
+  MovieDetailsState(this.movieID);
 
   @override
   void initState() {
     super.initState();
-    SecondHttpManager.getInstance().movieMessage(movieID,onSuccess: (data) {
-     var bean=data as MovieMessageBean;
+    SecondHttpManager.getInstance().movieMessage(movieID, onSuccess: (data) {
+      var bean = data as MovieMessageBean;
       print(bean.playState.toString());
 
-      setState((){
-
-      });
+      setState(() {});
     });
-
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('看看'),),
+      appBar: AppBar(
+        title: Text('看看'),
+      ),
       body: ListView(
         children: <Widget>[
           headItem(),
@@ -97,10 +94,13 @@ class MovieDetailsState extends State<MovieDetailsPage> {
                         fontWeight: FontWeight.bold,
                         fontSize: 30))
               ])),
-
               FlatButton.icon(
                 label: Text('俊俊'),
-                icon:  new Image.asset('image/icon_bool.png',width: 23,height: 23,),
+                icon: new Image.asset(
+                  'image/icon_bool.png',
+                  width: 23,
+                  height: 23,
+                ),
                 color: Colors.grey,
                 shape: StadiumBorder(),
                 onPressed: () {
@@ -111,7 +111,10 @@ class MovieDetailsState extends State<MovieDetailsPage> {
                   .intoExpanded(),
               FlatButton.icon(
                 label: Text('俊俊'),
-                icon: Icon(Icons.add_box,color: Colors.red,),
+                icon: Icon(
+                  Icons.add_box,
+                  color: Colors.red,
+                ),
                 color: Colors.grey,
                 shape: StadiumBorder(),
                 onPressed: () {},
@@ -120,25 +123,47 @@ class MovieDetailsState extends State<MovieDetailsPage> {
                   .intoExpanded(),
             ],
           ).intoPadding(padding: EdgeInsets.only(top: 13)),
-          Text('简介',style: TextStyle(color:Colors.black ,fontWeight: FontWeight.bold,fontSize: 18)).intoPadding(padding: EdgeInsets.only(top:10)),
-          Text("因故来到美国唐人街的叶问，意外卷入一场当地军方势力与华人武馆的纠纷，面对日益猖狂的民族歧视与压迫，叶问挺身而出，在美国海军陆战队军营拼死一战，以正宗咏春，向世界证明了中国功夫"),
-          Text('演职人员',style: TextStyle(color:Colors.black ,fontWeight: FontWeight.bold,fontSize: 18)).intoPadding(padding: EdgeInsets.only(top:10)),
+          Text('简介',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18))
+              .intoPadding(padding: EdgeInsets.only(top: 10)),
+          Text(
+              "因故来到美国唐人街的叶问，意外卷入一场当地军方势力与华人武馆的纠纷，面对日益猖狂的民族歧视与压迫，叶问挺身而出，在美国海军陆战队军营拼死一战，以正宗咏春，向世界证明了中国功夫"),
+          Text('演职人员',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18))
+              .intoPadding(padding: EdgeInsets.only(top: 10)),
           ListView(
             scrollDirection: Axis.horizontal,
             children: <Widget>[
-              Image.network("http://bj.people.com.cn//NMediaFile/2020/0313/LOCAL202003131750545452283166310.jpg",width: 85,height: 105,),
-              Image.network("http://bj.people.com.cn//NMediaFile/2020/0313/LOCAL202003131750545452283166310.jpg",width: 85,height: 105,),
-              Image.network("http://bj.people.com.cn//NMediaFile/2020/0313/LOCAL202003131750545452283166310.jpg",width: 85,height: 105,),
-              Image.network("http://bj.people.com.cn//NMediaFile/2020/0313/LOCAL202003131750545452283166310.jpg",width: 85,height: 105,),
+              Image.network(
+                "http://bj.people.com.cn//NMediaFile/2020/0313/LOCAL202003131750545452283166310.jpg",
+                width: 85,
+                height: 105,
+              ),
+              Image.network(
+                "http://bj.people.com.cn//NMediaFile/2020/0313/LOCAL202003131750545452283166310.jpg",
+                width: 85,
+                height: 105,
+              ),
+              Image.network(
+                "http://bj.people.com.cn//NMediaFile/2020/0313/LOCAL202003131750545452283166310.jpg",
+                width: 85,
+                height: 105,
+              ),
+              Image.network(
+                "http://bj.people.com.cn//NMediaFile/2020/0313/LOCAL202003131750545452283166310.jpg",
+                width: 85,
+                height: 105,
+              ),
             ],
           ).intoContainer(height: 110)
         ],
       ),
     );
-
   }
-
-
 }
-
-
