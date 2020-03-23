6bd1d03635b796108b6eafd298de4f994af0d558
li sao <1318213707@qq.com>
2020-03-23 23:35:46 +0800

    合并

--- lib/page/MovieDetailsPage.dart
+++ lib/page/MovieDetailsPage.dart
@@ -49,7 +49,7 @@ class MovieDetailsState extends State<MovieDetailsPage> {
     );
   }
 
-  Padding headItem(){
+  Padding headItem() {
     return Padding(
       padding: EdgeInsets.all(20),
       child: Column(
@@ -58,41 +58,66 @@ class MovieDetailsState extends State<MovieDetailsPage> {
           Row(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
-              Image.network("http://bj.people.com.cn//NMediaFile/2020/0313/LOCAL202003131750545452283166310.jpg",height: 150,width:110 ,),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('叶问',style: TextStyle(color: Colors.black,fontSize: 24,fontWeight: FontWeight.bold),),
-                      Text('动作/历史/剧情').intoContainer(margin: EdgeInsets.only(top: 10)),
-                      Text('甄子丹过招美军',style: TextStyle(color: Colors.black54,
-                          fontSize: 20,fontWeight: FontWeight.bold),).intoContainer(margin: EdgeInsets.only(top: 10))
-                    ],
-                  ).intoContainer(margin: EdgeInsets.all(10))
-                      .intoExpanded() ,
+              Image.network(
+                "http://bj.people.com.cn//NMediaFile/2020/0313/LOCAL202003131750545452283166310.jpg",
+                height: 150,
+                width: 110,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    '叶问',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text('动作/历史/剧情')
+                      .intoContainer(margin: EdgeInsets.only(top: 10)),
+                  Text(
+                    '甄子丹过招美军',
+                    style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ).intoContainer(margin: EdgeInsets.only(top: 10))
+                ],
+              ).intoContainer(margin: EdgeInsets.all(10)).intoExpanded(),
             ],
           ),
           Row(
             children: <Widget>[
-              Text.rich(TextSpan(
-                children: [
-                  TextSpan(text: '时光评分',style: TextStyle(color:Colors.cyan )),
-                  TextSpan(text: '7.7',style: TextStyle(color:Colors.cyan ,fontWeight: FontWeight.bold,fontSize: 30))
-                ]
-              )),
+              Text.rich(TextSpan(children: [
+                TextSpan(text: '时光评分', style: TextStyle(color: Colors.cyan)),
+                TextSpan(
+                    text: '7.7',
+                    style: TextStyle(
+                        color: Colors.cyan,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30))
+              ])),
 
-              FlatButton.icon(label: Text('俊俊'),icon: Icon(Icons.print),
-                color:Colors.grey ,
+              FlatButton.icon(
+                label: Text('俊俊'),
+                icon:  new Image.asset('image/icon_bool.png',width: 23,height: 23,),
+                color: Colors.grey,
                 shape: StadiumBorder(),
-                onPressed: (){
-
-              },).intoContainer(height: 30,padding:EdgeInsets.only(left: 20)).intoExpanded(),
-        FlatButton.icon(label: Text('俊俊'),icon: Icon(Icons.print),
-                color:Colors.grey ,
+                onPressed: () {
+                  print(AssetImage('image/icon_bool.png'));
+                },
+              )
+                  .intoContainer(height: 30, padding: EdgeInsets.only(left: 20))
+                  .intoExpanded(),
+              FlatButton.icon(
+                label: Text('俊俊'),
+                icon: Icon(Icons.add_box,color: Colors.red,),
+                color: Colors.grey,
                 shape: StadiumBorder(),
-                onPressed: (){
-
-              },).intoContainer(height: 30,padding:EdgeInsets.only(left: 20) ).intoExpanded(),
-
+                onPressed: () {},
+              )
+                  .intoContainer(height: 30, padding: EdgeInsets.only(left: 20))
+                  .intoExpanded(),
             ],
           ).intoPadding(padding: EdgeInsets.only(top: 13)),
           Text('简介',style: TextStyle(color:Colors.black ,fontWeight: FontWeight.bold,fontSize: 18)).intoPadding(padding: EdgeInsets.only(top:10)),
@@ -108,7 +133,7 @@ class MovieDetailsState extends State<MovieDetailsPage> {
             ],
           ).intoContainer(height: 110)
         ],
-      ) ,
+      ),
     );
 
   }
